5dc54bbb6614fdf73b1c9369120b5d98bf908115
jfbonvin <jfbonvin@protonmail.ch>
2022-06-15 11:25:58 +0200

    Delete CSH_Bulk.sh

--- data/quantitative/0cycle/CSH_Bulk.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH --nodes 7
-#SBATCH --ntasks 25
-#SBATCH --cpus-per-task 1
-#SBATCH --mem 40G
-#SBATCH --time 55:00:00
-module load intel
-module load intel-mpi
-srun ~/mylammps/lammps/src/lmp_mpi -in in.EricaFF2
